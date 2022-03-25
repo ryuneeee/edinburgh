fc3280bb53d88df1432ee293f06f4c42cf579939
Ryunhee Han <ryuneeee@gmail.com>
2022-03-26 04:46:30 +0900

    쿨엔조이 추가

--- src/site/coolnjoy.service.ts
+++ src/site/coolnjoy.service.ts
@@ -23,16 +23,21 @@ export class CoolnJoyService {
         const $ = cheerio.load(response.data);
         const articles = $('tbody>tr')
           .map((i, row) => {
-            const title = $(row).find('.td_subject').text().replace(/\n/g, '').replace(/댓글.*/g, '').trim();
+            const title = $(row)
+              .find('.td_subject')
+              .text()
+              .replace(/\n/g, '')
+              .replace(/댓글.*/g, '')
+              .trim();
             const href = baseUrl + $(row).find('.td_subject > a').attr('href');
             const hits = $(row).find('.td_hit').first().text().replace('.', '').replace('k', '00');
-            const comments = $(row).find('.cnt_cmt').first().text().replace('[', '').replace(']', '');;
+            const comments = $(row).find('.cnt_cmt').first().text().replace('[', '').replace(']', '');
 
             return { title: title, href: href, hits: Number(hits), comments: Number(comments) };
           })
           .get();
 
-          console.log(articles)
+        console.log(articles);
 
         this.articleService.scrap({ site: this.site, articles: articles });
       })
